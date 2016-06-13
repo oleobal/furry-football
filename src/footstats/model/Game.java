6a6afedcd20a1e13e66c14970d323354bcfc1aee
Jean Marc Abbas <jabbas@a-018598.ups.u-psud.fr>
2016-06-13 16:11:00 +0200

    Implemented file parsing Modified Snapshot class to add entries one by one
    Previous work got deleted because my accout was full... ( ͡° ʖ̯ ͡°)

--- src/footstats/model/Game.java
+++ src/footstats/model/Game.java
@@ -1,5 +1,14 @@
 package footstats.model;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.*;
+
 public class Game
 {
 	ArrayList<Snapshot> snapshots;
@@ -9,7 +18,77 @@ public class Game
 	 */
 	public Game(String input)
 	{
-		//TODO
+		snapshots = new ArrayList<Snapshot>();
+		
+		long start = System.currentTimeMillis();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(input))) {
+		    String line;
+		    
+		    System.out.println("Parsing data from " + input + "...");
+		    
+		    // Initialize date parser for every format possibilities
+		    SimpleDateFormat dateParserNoMillisec  = new SimpleDateFormat("\"yyyy-mm-dd hh:mm:ss\"");
+		    SimpleDateFormat dateParserOneMillisec = new SimpleDateFormat("\"yyyy-mm-dd hh:mm:ss.S\"");
+		    SimpleDateFormat dateParserTwoMillisec = new SimpleDateFormat("\"yyyy-mm-dd hh:mm:ss.SS\"");
+		    
+		    Snapshot currentSnapshot = null;
+		    Instant currentTimestamp = null;
+		    
+		    // for each line of the file
+		    while ((line = br.readLine()) != null) {
+		       
+		    	String data[] = line.split(",");
+		    
+		    	// choose the correct date parser for the timestamp
+		    	if(data[0].length() == dateParserNoMillisec.toPattern().length())
+		    	{
+		    		currentTimestamp = dateParserNoMillisec.parse(data[0]).toInstant();
+		    	}
+		    	else if(data[0].length() == dateParserOneMillisec.toPattern().length())
+		    	{
+		    		currentTimestamp = dateParserOneMillisec.parse(data[0]).toInstant();
+		    	}
+		    	else if(data[0].length() == dateParserTwoMillisec.toPattern().length())
+		    	{
+		    		currentTimestamp = dateParserTwoMillisec.parse(data[0]).toInstant();
+		    	}
+		    	else throw new ParseException("Unsupported date format in file " + input, 0);
+		    	
+		    	// if the parsed timestamp is different from the current snapshot's, create a new snapshot with that timestamp and add it to the list of snapshots
+		    	if(currentSnapshot == null || currentTimestamp.compareTo(currentSnapshot.getTimestamp()) > 0)
+		    	{
+		    		currentSnapshot = new Snapshot(currentTimestamp);
+		    		snapshots.add(currentSnapshot);
+		    	}
+		    	
+		    	// create and add trace to the current snapshot
+		    	currentSnapshot.addTrace(new Trace(Integer.parseInt(data[1]),
+		    									   Float.parseFloat(data[2]),
+		    									   Float.parseFloat(data[3]),
+		    									   Float.parseFloat(data[4]),
+		    									   Float.parseFloat(data[5]),
+		    									   Float.parseFloat(data[6]),
+		    									   Float.parseFloat(data[7]),
+		    									   Float.parseFloat(data[8])));
+		   
+		    }
+		    
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.err.println("Couldn't find file " + input);
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		} catch (ParseException e) {
+			System.err.println(e.getMessage());
+		}
+		
+		System.out.println("Finished in " + (System.currentTimeMillis() - start) + " ms. " + snapshots.size() + " snapshot entries added.");
+	}
+	
+	public static void main(String[] args)
+	{
+		Game g = new Game("data/2013-11-03_tromso_stromsgodset_first.csv");
 	}
 
 }
--- src/footstats/model/Snapshot.java
+++ src/footstats/model/Snapshot.java
@@ -1,6 +1,6 @@
 package footstats.model;
 
-import java.util.Date;
+import java.time.Instant;
 import java.util.HashMap;
 
 /**
@@ -8,16 +8,52 @@ import java.util.HashMap;
  */
 public class Snapshot
 {
-	private Date timestamp;
-	private HashMap<Integer, Trace> data;
+	private Instant timestamp;
+	private HashMap<Integer, Trace> data; // data for each player
 
-	public Snapshot(Date time, Trace[] input)
+	public Snapshot(Instant time)
 	{
 		timestamp = time;
+		data = new HashMap<Integer, Trace>();
+	}
 	
-		for (Trace lol: input)
-			data.add(lol.player, lol);
-
+	/**
+	 * Add the given trace to the Trace map of this snapshot.
+	 * @param t Trace to add
+	 */
+	public void addTrace(Trace t)
+	{
+		data.put(t.player, t);
+	}
+	
+	/**
+	 * Returns the Trace object describing the given player for this snapshot.
+	 * @param playerID ID of the player
+	 * @return Trace of the player, null if player isn't present in the snapshot
+	 */
+	public Trace getTraceOfPlayer(int playerID)
+	{
+		return data.get(playerID);
+	}
+	
+	/**
+	 * Getter.
+	 * @return Timestamp of the snapshot
+	 */
+	public Instant getTimestamp()
+	{
+		return timestamp;
+	}
+	
+	public String toString()
+	{
+		StringBuilder sb = new StringBuilder();
+		sb.append("Snapshot " + timestamp + "\n");
+		for(Trace t : data.values())
+		{
+			sb.append("	 " + t + "\n");
+		}
+		return sb.toString();
 	}
 
 }
